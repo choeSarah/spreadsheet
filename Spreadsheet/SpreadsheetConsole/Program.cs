a0a53826e4ce74a5b72dc4d8c5ba5ce6e67efa08
choeSarah <121065007+choeSarah@users.noreply.github.com>
2023-09-29 21:17:40 -0600

    fixing error in spreadsheetConsole

--- Spreadsheet/SpreadsheetConsole/Program.cs
+++ Spreadsheet/SpreadsheetConsole/Program.cs
@@ -4,15 +4,15 @@ using System.Xml.Linq;
 using SpreadsheetUtilities;
 using SS;
 
-try
-{
-    Spreadsheet s1 = new Spreadsheet("v1");
-    s1.SetContentsOfCell("1A", "Hello");
-    s1.Save("test1.txt");
-} catch (InvalidNameException)
-{
+//try
+//{
+//    Spreadsheet s1 = new Spreadsheet("v1");
+//    s1.SetContentsOfCell("1A", "Hello");
+//    s1.Save("test1.txt");
+//} catch (InvalidNameException)
+//{
 
-}
+//}
 
-Spreadsheet anotherOne = new Spreadsheet("/Users/sarahchoe/Projects/spreadsheet-choeSarah/Spreadsheet/test1.txt",
-    s => true, s => s.ToUpper(), "v1");
+//Spreadsheet anotherOne = new Spreadsheet("/Users/sarahchoe/Projects/spreadsheet-choeSarah/Spreadsheet/test1.txt",
+//    s => true, s => s.ToUpper(), "v1");
